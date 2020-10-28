786da8b6709e9cbe2a09bf62870677fe500c36e1
fkvl0327 <64388519+fkvl0327@users.noreply.github.com>
2020-10-28 13:25:29 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,9 +6,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		for (int i = 1; i < 10; i++) {
-			System.out.println(N+" * "+i+" = "+N*i);
+		int num = sc.nextInt();
+		for (int i = 0; i < num; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println(A+B);
 		}
+		sc.close();
 	}
 }
