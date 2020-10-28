d4bcfaa613cdf42144e5485be7d093192bda6509
fkvl0327 <64388519+fkvl0327@users.noreply.github.com>
2020-10-28 11:57:23 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,18 +6,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int H = sc.nextInt();
-		int M = sc.nextInt();
-		if (M >= 45) {
-			System.out.println(H + " " + (M - 45));
+		int N = sc.nextInt();
+		for (int i = 1; i < 10; i++) {
+			System.out.println(N * i);
 		}
-		if (H > 0 && M < 45) {
-			System.out.println((H - 1) + " " + (M - 45 + 60));
-		}
-		if (H == 0 && M < 45) {
-			System.out.println(23 + " " + (M - 45 + 60));
-		}
-
 	}
-
 }
