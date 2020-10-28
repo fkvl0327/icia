c8958c5d5b7f26c0cb3b4c1d4a3b2d29a55dd4f1
fkvl0327 <64388519+fkvl0327@users.noreply.github.com>
2020-10-28 13:30:00 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,12 +6,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		for (int i = 0; i < num; i++) {
-			int A = sc.nextInt();
-			int B = sc.nextInt();
-			System.out.println(A+B);
+		int n = sc.nextInt();
+		int sum = 0;
+		for(int i=1; i<n+1; i++) {
+			sum+=i;
 		}
-		sc.close();
+		System.out.println(sum);
 	}
 }
