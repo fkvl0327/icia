19911620c02980d6b85b1f748f7e57f55cf646aa
fkvl0327 <64388519+fkvl0327@users.noreply.github.com>
2020-10-28 16:54:42 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,34 @@
 package wBaekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int sum = 0;
-		for(int i=1; i<n+1; i++) {
-			sum+=i;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader
+				(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter
+				(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		//BufferedReader로 연산을 몇 번 할 것인지 입력받는다
+		//문자열을 입력받았기 때문에 숫자로 인식하기 위해 Integer.parseInt가 필요
+		int T= Integer.parseInt(br.readLine());
+		
+		for(int i=0;i<T;i++) {
+			//한 줄에 숫자를 두 개 띄어쓰기해서 입력받는다
+			//입력된 숫자는 띄어쓰기를 제외하고 인식하기로 한다
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			//입력된 두 개의 숫자를 더한 값을
+			int sum = A+B;
+			//BufferedWriter로 출력한다
+			bw.write(sum + "\n");
 		}
-		System.out.println(sum);
+		bw.flush();
 	}
 }
