1b9cd7df5f2aa9c2ee8b8c75b23d116755a22295
fkvl0327 <64388519+fkvl0327@users.noreply.github.com>
2020-11-05 08:26:45 +0900

    Add files via upload

--- ChildBeads.java
+++ ChildBeads.java
@@ -2,37 +2,25 @@ package test;
 
 public class ChildBeads {
 	private int beads;
-	
+
 	public ChildBeads(int beads) {
 		this.beads = beads;
 	}
-	
-	public void obtainBead(ChildBeads child, int num) {
-		if(child.beads<num) {
-			this.beads += child.beads;
-		}else {
-			this.beads += num;
-		}
-		child.loseBead(num);
-		
-	}
 
-	private int loseBead(int num) {
-		if(beads < num) {
-			beads = 0;
-		}else {
-			beads -= num;
+	public void obtainBead(ChildBeads child, int beads) {
+		if (child.beads < beads) {
+			this.beads += child.beads;
+			child.beads = 0;
+		} else {
+			this.beads += beads;
+			child.beads -= beads;
 		}
-		return beads;
+
 	}
 
 	public void showBeadsNum() {
-		System.out.println("beads="+ beads);
-		
-		
+		System.out.println("beads=" + beads);
+
 	}
-	
-	
-	
 
 }
